5d758a9f2abfa18d3b81a8d320421078e42cbd13
hasan <hassanhitleap1@gmail.com>
2019-04-25 17:05:55 +0300

    lesson 5

--- wp-content/plugins/new_plgin/index.php
+++ wp-content/plugins/new_plgin/index.php
@@ -7,3 +7,43 @@
 * Author: by hasan kiwan
 * Author URI: http://localhost/
 **/
+
+defined('ABSPATH') or die('exit ya baby ');
+
+
+class AlecaddPlgin{
+    function __construct( ) {
+        add_action('init',[$this,'custum_post_type']) ;
+    }
+
+    public function activate(){
+        $this->custum_post_type();
+        flush_rewrite_rules();
+    }
+
+    public function deactivate(){
+        flush_rewrite_rules();
+    }
+
+    public function uninstall(){
+
+    }
+
+
+    public function custum_post_type(){
+        register_post_type('book',['public'=>true,'label'=>'books']);
+    }
+}
+
+if(class_exists('AlecaddPlgin')){
+ $alecaddPlgin= new AlecaddPlgin();
+}
+
+// activate hook
+register_activation_hook(__FILE__,array($alecaddPlgin,'activate'));
+
+// deactivate hook
+register_deactivation_hook(__FILE__,array($alecaddPlgin,'deactivate'));
+
+
+
