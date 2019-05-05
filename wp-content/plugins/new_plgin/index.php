c32792b874aa6a32ad0c5ae34e08e884e3bc381e
hasan <hassanhitleap1@gmail.com>
2019-05-05 16:34:28 +0300

    lesson 7

--- wp-content/plugins/new_plgin/index.php
+++ wp-content/plugins/new_plgin/index.php
@@ -16,6 +16,10 @@ class AlecaddPlgin{
         add_action('init',[$this,'custum_post_type']) ;
     }
 
+    public function register(){
+        add_action('admin_enqueue_scripts',[$this,'enqueue']) ;//admin_enqueue_scripts  admin panel // wp_enqueue_scripts for users 
+    }
+
     public function activate(){
         $this->custum_post_type();
         flush_rewrite_rules();
@@ -33,10 +37,17 @@ class AlecaddPlgin{
     public function custum_post_type(){
         register_post_type('book',['public'=>true,'label'=>'books']);
     }
+
+    public function enqueue(){
+        // enqueue ouer secrpts 
+        wp_enqueue_style('myplginstyle', plugins_url('/assets/mystyle.css',__FILE__)); // $deps = array(), $ver = false, $media = 'all'
+        wp_enqueue_script('myplginscript', plugins_url('/assets/myjs.css',__FILE__));
+    }
 }
 
 if(class_exists('AlecaddPlgin')){
  $alecaddPlgin= new AlecaddPlgin();
+ $alecaddPlgin->register();
 }
 
 // activate hook
