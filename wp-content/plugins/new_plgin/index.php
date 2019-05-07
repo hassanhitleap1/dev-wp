f0e8de689d14191c7e2afa7d78073a9ac379757f
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-05-07 12:18:08 -0700

    commit

--- wp-content/plugins/new_plgin/index.php
+++ wp-content/plugins/new_plgin/index.php
@@ -10,67 +10,62 @@
 
 defined('ABSPATH') or die('exit ya baby ');
 
+if (class_exists('AlecaddPlgin')) {
+    class AlecaddPlgin{
+        function __construct( ) {
+        
+        }
 
-class AlecaddPlgin{
-    function __construct( ) {
-       
+        public  function register(){
+            add_action('admin_enqueue_scripts',[ 'AlecaddPlgin','enqueue']) ;//admin_enqueue_scripts  admin panel // wp_enqueue_scripts for users 
+        }
+
+        public  function activate(){
+            //  $this->custum_post_type();
+            // activate hook
+            require_once plugin_dir_url(__FILE__) . 'inc/new-plugin-activation.php';
+            register_activation_hook(__FILE__,array( 'NewPluginActivation','activate'));
+        }
+
+        public static function deactivate(){
+            // deactivate hook
+            require_once plugin_dir_url(__FILE__) . 'inc/new-plugin-deactivation.php';
+            register_deactivation_hook(__FILE__,array( 'NewPluginDeactivation','deactivate'));
+        }
+
+        protected   function create_posts_type(){
+            add_action('init',[$this,'custum_post_type']) ;
+        }
+        public static function uninstall(){
+
+        }
+
+
+        public  static function custum_post_type(){
+            register_post_type('book',['public'=>true,'label'=>'books']);
+        }
+
+        public static function enqueue(){
+            // enqueue ouer secrpts 
+            wp_enqueue_style('myplginstyle', plugins_url('/assets/mystyle.css',__FILE__)); // $deps = array(), $ver = false, $media = 'all'
+            wp_enqueue_script('myplginscript', plugins_url('/assets/myjs.css',__FILE__));
+        }
+
     }
 
-    public static function register(){
-        add_action('admin_enqueue_scripts',[ 'AlecaddPlgin','enqueue']) ;//admin_enqueue_scripts  admin panel // wp_enqueue_scripts for users 
-    }
+     $alecaddPlgin= new AlecaddPlgin();
+     $alecaddPlgin->register();
 
-    public static function activate(){
-        $this->custum_post_type();
-        flush_rewrite_rules();
-    }
+    register_activation_hook(__FILE__, array( $alecaddPlgin, 'activate'));
 
-    public static function deactivate(){
-        flush_rewrite_rules();
-    }
-
-    protected  static function create_posts_type(){
-        add_action('init',[$this,'custum_post_type']) ;
-    }
-    public static function uninstall(){
-
-    }
+    register_deactivation_hook(__FILE__, array($alecaddPlgin, 'deactivate'));
 
 
-    public  static function custum_post_type(){
-        register_post_type('book',['public'=>true,'label'=>'books']);
-    }
 
-    public static function enqueue(){
-        // enqueue ouer secrpts 
-        wp_enqueue_style('myplginstyle', plugins_url('/assets/mystyle.css',__FILE__)); // $deps = array(), $ver = false, $media = 'all'
-        wp_enqueue_script('myplginscript', plugins_url('/assets/myjs.css',__FILE__));
-    }
 
-}
 
-// class SecoundClass extends AlecaddPlgin{
-//     public function register_post_type(){
-//         $this->create_posts_type();
-//     } 
-// }
 
-if(class_exists('AlecaddPlgin')){
-//  $alecaddPlgin= new AlecaddPlgin();
-//  $alecaddPlgin->register();
-    AlecaddPlgin::register();
-}
-
-// $secoundclass= new SecoundClass();
-// $secoundclass->register_post_type();
-
-// activate hook
-require_once plugin_dir_url(__FILE__). 'inc/new-plugin-activation.php';
-//register_activation_hook(__FILE__,array( 'NewPluginActivation','activate'));
-
-// deactivate hook
-require_once plugin_dir_url(__FILE__) . 'inc/new-plugin-deactivation.php';
-//register_deactivation_hook(__FILE__,array( 'NewPluginDeactivation','deactivate'));
 
 //register_uninstall_hook(__FILE__,array($alecaddPlgin,'uninstall'));
+}
 
